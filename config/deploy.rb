e83a5a5e539c34d15fd15650bb54cb65d48724f8
remcohh <remcohh@gmail.com>
2019-05-19 21:16:01 +0200

    add application settings as static file

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'git@github.com:remcohh/band-aye.git'
 
 set :deploy_to, '/home/deploy/servatius'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/audios public/scans public/images}
 
 namespace :deploy do
