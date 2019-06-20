daa67099674a49119da2790af1e3e528c487091a
remcohh <remcohh@gmail.com>
2019-06-20 23:08:47 +0200

    improve show form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def status_color(status)
+    case status
+      when 'Onbekend'
+        return 'blue'
+      when 'Aangemeld'
+        return 'green'
+      when 'Afgemeld'
+        return 'red'
+    end
+
+  end
+
 end
