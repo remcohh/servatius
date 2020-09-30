e6e3aeab371147d779a87efa06c2787878f3f76c
Remco <remcohh@gmail.com>
2020-09-30 16:01:52 +0200

    update server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '52.232.39.167', user: 'remco', roles: %w{web app db}
+server '51.144.75.61', user: 'remco', roles: %w{web app db}
